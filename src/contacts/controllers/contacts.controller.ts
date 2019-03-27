8c96211af9f62d3a37286f35d7ffb1f1bed5edfe
Vatsaev Aslan <a.vatsaev@gmail.com>
2019-03-27 17:18:32 +0100

    feat: implemented nestjs crud

--- src/contacts/controllers/contacts.controller.ts
+++ src/contacts/controllers/contacts.controller.ts
@@ -8,31 +8,4 @@ import { Crud } from '@nestjsx/crud';
 export class ContactsController {
 
     constructor(public readonly service: ContactsService) {}
-
-    // @Get()
-    // async index() {
-    //     return await this.contactsService.index();
-    // }
-    //
-    // @Get(':id')
-    // async show(@Param('id') id) {
-    //     return await this.contactsService.show(id);
-    // }
-    //
-    // @Post()
-    // async create(@Body() contact: ContactEntity) {
-    //     return await this.contactsService.create(contact);
-    // }
-    //
-    // @Patch(':id')
-    // async update(@Body() contactData: ContactEntity, @Param('id') id) {
-    //     return await this.contactsService.update(id, contactData);
-    // }
-    //
-    // @HttpCode(204)
-    // @Delete(':id')
-    // async destroy(@Param('id') id) {
-    //     await this.contactsService.delete(id);
-    //     return;
-    // }
 }
