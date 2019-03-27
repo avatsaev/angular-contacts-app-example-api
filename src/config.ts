f3156987b6f4b3c6d799b8705226a49faf1c5680
Vatsaev Aslan <a.vatsaev@gmail.com>
2019-03-27 15:05:33 +0100

    feat: enable socket scaling with redis adapter

--- src/config.ts
+++ src/config.ts
@@ -15,18 +15,18 @@ export const config: AppConfiguration = {
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: Number(process.env.REDIS_PORT) || 6379,
-    enabled: process.env.REDIS_ENABLED || true
+    enabled: process.env.REDIS_ENABLED || false
   },
   database: {
     type: 'postgres',
     host: process.env.POSTGRES_HOST         || 'localhost',
     port: Number(process.env.POSTGRES_PORT) || 5432,
-    username: process.env.POSTGRES_USERNAME || 'contacts_db',
+    username: process.env.POSTGRES_USER || 'contacts_db',
     password: process.env.POSTGRES_PASSWORD || 'contacts_db_pass',
     database: process.env.POSTGRES_DB       || 'contacts_db',
     entities: [`${__dirname}/**/entities/*.{js,ts}`],
     synchronize: true,
-    replication: process.env.DB_MASTER_URL ? {
+    replication: process.env.DB_MASTER_URL && process.env.DB_REPLICAS_URL ? {
       master: {
         url:  process.env.DB_MASTER_URL,
       },
