255095ffe3ba268942046c9130108960ce64fb47
Aslan Vatsaev <a.vatsaev@gmail.com>
2019-03-29 00:39:50 +0100

    Update redis-io.adapter.ts

--- src/core/adapters/redis-io.adapter.ts
+++ src/core/adapters/redis-io.adapter.ts
@@ -4,7 +4,7 @@ import { config } from '../../config';
 
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
-    const redisAdapter = redisIoAdapter({ host: config.redis.host, port: config.red$
+    const redisAdapter = redisIoAdapter({ host: config.redis.host, port: config.redis.port});
     const server = super.createIOServer(port, options);
     server.adapter(redisAdapter);
     return server;
