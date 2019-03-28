d8c9d0371f81c271e5f3e12d8954f752d753d260
A. Vatsaev <a.vatsaev@gmail.com>
2019-03-29 00:36:08 +0100

    fix: socket adap

--- src/core/adapters/redis-io.adapter.ts
+++ src/core/adapters/redis-io.adapter.ts
@@ -2,10 +2,9 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as redisIoAdapter from 'socket.io-redis';
 import { config } from '../../config';
 
-const redisAdapter = redisIoAdapter({ host: config.redis.host, port: config.redis.port });
-
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: any): any {
+    const redisAdapter = redisIoAdapter({ host: config.redis.host, port: config.red$
     const server = super.createIOServer(port, options);
     server.adapter(redisAdapter);
     return server;
