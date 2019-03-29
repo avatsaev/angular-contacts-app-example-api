a71ddadef4da836ae84eea042efe99e23536aebb
Vatsaev Aslan <a.vatsaev@gmail.com>
2019-03-29 12:37:52 +0100

    chore: remove unused file

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +7,6 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -16,7 +14,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toEqual({
+        status: 'ok'
+      });
     });
   });
 });
--- src/contacts/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-import * as redisAdapter from 'socket.io-redis';
-
-export const getRedisAdapter = (redisHost?: string, redisPort = 6379) => redisHost ? redisAdapter({
-        host: redisHost,
-        port: redisPort
-    }) : null;
